b24b92f39695649f474af152cfd1601c2a4600c7
sunwng <sunwgna@gmail.com>
2024-02-10 14:38:01 +0900

    init

---
+++ src/main/java/org/example/studyspringcore/StudySpringCoreApplication.java
@@ -0,0 +1,13 @@
+package org.example.studyspringcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringCoreApplication.class, args);
+    }
+
+}
